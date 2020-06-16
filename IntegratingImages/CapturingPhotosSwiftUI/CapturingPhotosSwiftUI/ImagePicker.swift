1bf295cb5417fd5faf6f9d88873585c996f0ecb1
Yasmin Cropanisi <yasmin.cropanisi@arctouch.com>
2020-06-16 16:24:05 -0300

    Integrating camera

---
+++ IntegratingImages/CapturingPhotosSwiftUI/CapturingPhotosSwiftUI/ImagePicker.swift
@@ -0,0 +1,9 @@
+//
+//  ImagePicker.swift
+//  CapturingPhotosSwiftUI
+//
+//  Created by Yasmin Cropanisi on 16/06/20.
+//  Copyright © 2020 Yasmin Cropanisi. All rights reserved.
+//
+
+import Foundation
---
+++ IntegratingImages/CapturingPhotosSwiftUI/CapturingPhotosSwiftUI/PhotoCapturedView.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCapturedView.swift
+//  CapturingPhotosSwiftUI
+//
+//  Created by Yasmin Cropanisi on 16/06/20.
+//  Copyright © 2020 Yasmin Cropanisi. All rights reserved.
+//
+
+import SwiftUI
+
+struct PhotoCapturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhotoCapturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCapturedView()
+    }
+}
