72016868467ca68851317fb1d12deab2d8e4da93
Yasmin Cropanisi <yasmin.cropanisi@arctouch.com>
2020-06-16 20:48:03 -0300

    Custom view animation

--- SwiftUIBasicAnimation/SwiftUIBasicAnimation/ContentView.swift
+++ SwiftUIBasicAnimation/SwiftUIBasicAnimation/ContentView.swift
@@ -12,17 +12,32 @@ struct ContentView: View {
   
   @State private var showDetails: Bool = false
   var body: some View {
-    VStack {
+    Group {
+    VStack(alignment: .center) {
+      
+      Text("Notification")
+        .frame(width: 400, height: 100)
+        .foregroundColor(Color.white)
+        .background(Color.green)
+        .cornerRadius(10)
+        .offset(y: showDetails ?  0 : -200 )
+        .animation(.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 10, initialVelocity: 0))
+        
+      Spacer()
+      
       Button("Press me") {
         self.showDetails.toggle()
       }
-      HStack {
-        Text("🛬")
-          .font(.custom("Arial", size: 100))
-          .offset(x: self.showDetails ? UIScreen.main.bounds.width - 120 : 0)
-          .animation((.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 10, initialVelocity: 0)))
-        Spacer()
-      }
+      
+      Spacer()
+//      HStack {
+//        Text("🛬")
+//          .font(.custom("Arial", size: 100))
+//          .offset(x: self.showDetails ? UIScreen.main.bounds.width - 120 : 0)
+//          .animation((.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 10, initialVelocity: 0)))
+//        Spacer()
+//      }
+    }
     }
   }
 }
