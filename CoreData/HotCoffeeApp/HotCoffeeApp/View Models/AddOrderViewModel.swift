b49786e4b3eb5b82105da3cc04412dfd2311c170
Yasmin Cropanisi <yasmin.cropanisi@arctouch.com>
2020-06-16 14:20:17 -0300

    Deleting orders

---
+++ CoreData/HotCoffeeApp/HotCoffeeApp/View Models/AddOrderViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddOrderViewModel.swift
+//  HotCoffeeApp
+//
+//  Created by Yasmin Cropanisi on 16/06/20.
+//  Copyright © 2020 Yasmin Cropanisi. All rights reserved.
+//
+
+import Foundation
---
+++ CoreData/HotCoffeeApp/HotCoffeeApp/Views/AddOrderView.swift
@@ -0,0 +1,21 @@
+//
+//  AddOrderView.swift
+//  HotCoffeeApp
+//
+//  Created by Yasmin Cropanisi on 16/06/20.
+//  Copyright © 2020 Yasmin Cropanisi. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddOrderView()
+    }
+}
